826ae29b150c4a08324caa527fcd4734ab5eb1f5
Tursynkhan <t_karimkhanov@kbtu.kz>
2023-01-27 19:32:58 +0600

    Variable Names

---
+++ week1/Variable Names/ex5.py
@@ -0,0 +1,23 @@
+"""Variable Names"""
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Illegal variable names:
+
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+myVariableName = "John"
+
+MyVariableName = "John"
+
+my_variable_name = "John"
+
+
