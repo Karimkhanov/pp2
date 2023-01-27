e7756b3d95644c450fd2caa508b704626d805123
Tursynkhan <t_karimkhanov@kbtu.kz>
2023-01-27 19:23:36 +0600

    Python Variables

---
+++ ex2.py
@@ -0,0 +1,27 @@
+"""PYTHON SYNTAX"""
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+#Syntax Error:
+#if 5 > 2:
+#print("Five is greater than two!")
+
+if 5 > 2:
+    print('Five is greater than two!')
+if 5 > 2:
+    print('Five is greater than two!')
+
+#     Syntax Error:
+# if 5 > 2:
+#  print("Five is greater than two!")
+#         print("Five is greater than two!")
+
+#VARIABLES IN PYTHON:
+x = 5
+y = 'Hello, World!'
+
+#COMMENTS IN PYTHON 
+#This is a comment.
+print('Hello,World!')
+
---
+++ ex4.py
@@ -0,0 +1,33 @@
+"""PYTHON VARIABLES"""
+
+#PYTHON VARIABLES
+x = 5
+y = 'John'
+print(x)
+print(y)
+
+x = 4
+x = 'Sally'
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = 'John'
+print(type(x))
+print(type(y))
+
+x = 'John '
+#is the same as 
+x = "John"
+
+a = 4
+A = 'Sally'
+print(a)
+print(A)
+
