9dbad0527ece7de69a24131416c8325c9d97c2f7
Tursynkhan <t_karimkhanov@kbtu.kz>
2023-02-03 21:34:35 +0600

    Exercises

---
+++ lab2/Python While loops/ex1.py
@@ -0,0 +1,5 @@
+#Print i as long as i is less than 6:
+i = 1
+while i < 6:
+  print(i)
+  i += 1
---
+++ lab2/Python While loops/ex2.py
@@ -0,0 +1,7 @@
+#Exit the loop when i is 3:
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
