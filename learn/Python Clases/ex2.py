07e95ddeb2d6b820cf54fefbf9b91beae9644b65
Karimkhanov <123242644+Karimkhanov@users.noreply.github.com>
2023-02-27 21:41:40 +0600

    Delete learn directory

--- learn/Python Clases/ex2.py
+++
@@ -1,7 +0,0 @@
-#Create an object named p1, and print the value of x:
-class MyClass:
-  x = 5
-
-p1 = MyClass()
-print(p1.x)
-
--- learn/Python Clases/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-a = input('Enter a text: ')
-# for i in a:
-#   if i == ',' or i == ' ':
-#     print(i, end = '')
-#   else:
-#     print(i*2, end = '')
